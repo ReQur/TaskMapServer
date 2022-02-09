cf4c51784eb78ca1e094024aa888495727d354f4
Danil Khabibullin <dkhabibullin@cloudlinux.com>
2022-02-09 13:57:10 +0500

    Init

---
+++ UserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace dotnetserver
+{
+    public interface ITimeService
+    {
+        DateTime GetCurrentTime();
+    }
+
+    public class TimeService: ITimeService
+    {
+        public DateTime GetCurrentTime()
+        {
+            return DateTime.Now;
+        }
+    }
+}
