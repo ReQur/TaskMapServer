c2d349b7dc51f802eebfe04bf780a8df19d54582
Danil Khabibullin <dkhabibullin@cloudlinux.com>
2022-03-09 17:57:55 +0500

    Add MainHubController and Models folder

---
+++ Controllers/MainHubController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace dotnetserver.Controllers
+{
+    public class MainHub: Hub
+    {
+        private static readonly Dictionary<string, string> ConnectionsGroup = new Dictionary<string, string>();
+
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            if (ConnectionsGroup.ContainsKey(Context.ConnectionId))
+            {
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, ConnectionsGroup[Context.ConnectionId]);
+                ConnectionsGroup.Remove(Context.ConnectionId);
+            }
+            await base.OnDisconnectedAsync(exception);
+        }
+        public async Task JoinGroup(string group)
+        {
+            if (ConnectionsGroup.ContainsKey(Context.ConnectionId))
+            {
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, ConnectionsGroup[Context.ConnectionId]);
+                ConnectionsGroup.Remove(Context.ConnectionId);
+            }
+            ConnectionsGroup.Add(Context.ConnectionId, group);
+            await Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+
+        public async Task NewMessage(string groupName, string message)
+        {
+            await Clients.OthersInGroup(groupName).SendAsync("newMessage", message);
+        }
+    }
+}
