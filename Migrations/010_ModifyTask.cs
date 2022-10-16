25af9b930d5a9e6f6c7dfc9089066bbb3a363b3f
Danil Khabibullin <dkhabibullin@cloudlinux.com>
2022-10-16 16:16:25 +0500

    modify task model and service
    modify task:
    	delete coordinates from task model
    	add x and y fileds to task model
    	add next_task_id to task model
    
    modify task service:
    	change insert task query
    	change delete task query

---
+++ Migrations/010_ModifyTask.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+
+namespace dotnetserver
+{
+    [Migration(010)]
+    public class ModifyTask_010 : Migration
+    {
+        public override void Down()
+        {
+            Execute.Script("./Migrations/sql/Down.010.ModifyTask.sql");
+        }
+        public override void Up()
+        {
+            Execute.Script("./Migrations/sql/Up.010.ModifyTask.sql");
+        }
+        
+    }
+}
