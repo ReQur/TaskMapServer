3bc7eca7a448490099f40c44491c57fb2b959ef5
dkhabibullin <dkhabibullin@cloudlinux.com>
2022-10-21 15:54:11 +0500

    Add TaskList class that provides sorted tasks

---
+++ Models/TaskList .cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace dotnetserver.Models
+{
+    public class ITaskList
+    {
+        protected BoardTask[] tasks;
+    }
+
+    public class TaskList : ITaskList
+    {
+        public TaskList(IEnumerable<BoardTask> _tasks)
+        {
+            var count = _tasks.Count();
+            tasks = new BoardTask[count];
+            BoardTask t = _tasks.Single(x => x.next_task_id == 0);
+            tasks[0] = t;
+            for (int i = 1; i < count; i++)
+            {
+                t = _tasks.Single(x => x.next_task_id == t.taskId);
+                tasks[i] = t;
+            }
+        }
+
+        public BoardTask[] GetTasks() => tasks;
+        
+    }
+
+}
