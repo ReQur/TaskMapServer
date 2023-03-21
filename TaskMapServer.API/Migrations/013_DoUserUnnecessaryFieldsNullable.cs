9b75d1091899b7732c7343bd00bbb6e1f14d48fe
dkhabibullin <dkhabibullin@cloudlinux.com>
2023-03-21 20:37:34 +0500

    Update user model
    Do firsName lastName fields nullable

---
+++ TaskMapServer.API/Migrations/013_DoUserUnnecessaryFieldsNullable.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+
+namespace dotnetserver
+{
+    [Migration(013)]
+    public class DoUserUnnecessaryFieldsNullable_013 : Migration
+    {
+        public override void Down()
+        {
+            Execute.Script("./Migrations/sql/Down.013.DoUserUnnecessaryFieldsNullable.sql");
+        }
+        public override void Up()
+        {
+            Execute.Script("./Migrations/sql/Up.013.DoUserUnnecessaryFieldsNullable.sql");
+        }
+        
+    }
+}
