55901c9d9073617710f8484d1371791c829f866f
Danil Khabibullin <dkhabibullin@cloudlinux.com>
2023-05-11 19:43:41 +0500

    Related to previous commit

---
+++ TaskMapServer.API/BoardNotificationHub/BoardNotificationsHub.cs
@@ -0,0 +1,88 @@
+﻿using dotnetserver.Models;
+using Microsoft.AspNetCore.SignalR;
+using System.Net;
+using System.Threading.Tasks;
+using Ubiety.Dns.Core;
+
+namespace dotnetserver.BoardNotificationHub
+{
+    public class BoardNotificationsHub : Hub
+    {
+        private readonly IUserService _userService;
+        private readonly IBoardService _boardService;
+
+        public BoardNotificationsHub(IUserService userService, IBoardService boardService)
+        {
+            _userService = userService;
+            _boardService = boardService;
+        }
+
+        /// <summary>
+        /// Joins a user to a specific board.
+        /// </summary>
+        /// <remarks>
+        /// This endpoint allows a user to join a specific board. The user will then receive updates and notifications related to the board.
+        ///
+        /// Access to a board is restricted based on the user's access level for that board. If the user does not have read permissions for the board, they will be unable to join.
+        ///
+        /// A cookie named "boardGroup" will be set in the user's browser. This cookie will be used to keep track of the user's current board.
+        ///
+        /// The server will also join the user to the board's SignalR group. This enables the server to send real-time updates to the user about the board.
+        ///
+        /// This method is idempotent - it can be called multiple times without changing the result. However, it is not recommended to call this method unnecessarily, as it may cause unnecessary server load.
+        ///
+        /// </remarks>
+        /// <param name="boardId">The ID of the board the user wishes to join.</param>
+        /// <returns>An HTTP 200 OK status code if the operation was successful.</returns>
+        /// <response code="200">Join successful.</response>
+        /// <response code="403">If the user does not have read permissions for the board.</response>
+        /// <response code="500">If an internal server error occurs.</response>
+        public async Task JoinBoard(string groupName)
+        {
+            var userId = await _userService.GetUserId(Context.User?.Identity?.Name);
+            try
+            {
+                // Проверка прав доступа пользователя
+                var userAccessLevel = await _boardService.GetUserAccessLevel(userId, uint.Parse(groupName));
+                if (!BoardPermissions.canRead(userAccessLevel))
+                {
+                    throw new HubException("Insufficient permissions");
+                }
+                await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception if needed
+                throw new HubException("Internal server error. Please try again later.");
+            }
+        }
+
+        /// <summary>
+        /// Allows a user to leave a previously joined board.
+        /// </summary>
+        /// <remarks>
+        /// This method is used when a user wants to stop receiving notifications from a specific board.
+        /// It uses a cookie named "boardGroup" to identify the board which the user wants to leave.
+        /// If the cookie is not found or the operation fails, an error message is returned.
+        /// </remarks>
+        /// <returns>
+        /// Returns a status of 200 (OK) if the operation is successful.
+        /// Returns a status of 500 (Internal Server Error) if an unexpected error occurs.
+        /// </returns>
+        /// <response code="200">Successfully left the board.</response>
+        /// <response code="500">Internal server error. Please try again later.</response>
+        public async Task LeaveBoard(string groupName)
+        {
+            try
+            {
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception if needed
+                throw new HubException("Internal server error. Please try again later.");
+            }
+        }
+    }
+}
+
---
+++ TaskMapServer.API/BoardNotificationHub/SendNotificationAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace dotnetserver.BoardNotificationHub
+{
+    public class SendNotificationAttribute : ActionFilterAttribute
+    {
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next.Invoke();
+            // Exit the filter if the result is not OkResult
+            if (!(resultContext.Result is OkResult))
+            {
+                return;
+            }
+            // Getting the boardGroup from cookies after the action method is executed
+            string boardGroup;
+            if (context.HttpContext.Request.Cookies.TryGetValue("boardGroup", out boardGroup))
+            {
+                // Get IHubContext via Service Locator
+                var hubContext = context.HttpContext.RequestServices.GetRequiredService<IHubContext<BoardNotificationsHub>>();
+
+                // Invoke SendNotificationToGroup method of the hub
+                await hubContext.Clients.Group(boardGroup).SendAsync("ReceiveNotification", boardGroup);
+            }
+        }
+    }
+}
+
