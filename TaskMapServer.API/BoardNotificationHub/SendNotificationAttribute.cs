3297e605c4948afff4d1bfbf27d55c4e6b73f11e
Danil Khabibullin <dkhabibullin@cloudlinux.com>
2023-06-10 22:43:30 +0500

    Change using cookies to headres

--- TaskMapServer.API/BoardNotificationHub/SendNotificationAttribute.cs
+++ TaskMapServer.API/BoardNotificationHub/SendNotificationAttribute.cs
@@ -11,20 +11,25 @@ namespace dotnetserver.BoardNotificationHub
         {
             var resultContext = await next.Invoke();
             // Exit the filter if the result is not OkResult
-            if (!(resultContext.Result is OkResult))
+            if (!(context.HttpContext.Response.StatusCode == 200))
             {
                 return;
             }
             // Getting the boardGroup from cookies after the action method is executed
             string boardGroup;
-            if (context.HttpContext.Request.Cookies.TryGetValue("boardGroup", out boardGroup))
+            try
             {
+                boardGroup = context.HttpContext.Request.Headers["Board-Group"];
                 // Get IHubContext via Service Locator
                 var hubContext = context.HttpContext.RequestServices.GetRequiredService<IHubContext<BoardNotificationsHub>>();
-
                 // Invoke SendNotificationToGroup method of the hub
                 await hubContext.Clients.Group(boardGroup).SendAsync("ReceiveNotification", boardGroup);
             }
+            catch (Exception ex)
+            {
+                return;
+            }
+            
         }
     }
 }
