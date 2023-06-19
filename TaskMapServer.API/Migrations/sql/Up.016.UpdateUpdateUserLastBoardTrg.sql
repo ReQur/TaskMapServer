4c2e89ccce73027b7ae811b2644c7cbd0cb2b84a
Danil Khabibullin <dkhabibullin@cloudlinux.com>
2023-06-19 21:02:41 +0500

    Fix last board trigger

--- TaskMapServer.API/Migrations/sql/Up.016.UpdateUpdateUserLastBoardTrg.sql
+++ TaskMapServer.API/Migrations/sql/Up.016.UpdateUpdateUserLastBoardTrg.sql
@@ -3,17 +3,11 @@ DROP TRIGGER UpdateUserLastBoard_trg;
 CREATE TRIGGER UpdateUserLastBoard_trg BEFORE DELETE ON board
     FOR EACH ROW 
     BEGIN
-        DECLARE _userId INT UNSIGNED DEFAULT NULL;
-        SELECT userId
-        INTO _userId
-        FROM user
-        WHERE lastBoardId = OLD.boardId;
-
-        IF _userId
-        THEN UPDATE user
-             SET lastBoardId = (SELECT MIN(boardId)
-                                FROM user_to_board
-                                WHERE userId IN(SELECT _userId) AND boardId != OLD.boardId)
-             WHERE userId IN(SELECT _userId);
-        END IF;
+        UPDATE
+             user
+        SET lastBoardId = ( SELECT MIN(boardId)
+                            FROM user_to_board
+                            WHERE userId IN(SELECT userId FROM user WHERE lastBoardId = OLD.boardId)
+                            AND boardId != OLD.boardId)
+        WHERE userId IN(SELECT userId FROM user WHERE lastBoardId = OLD.boardId);
     END;
